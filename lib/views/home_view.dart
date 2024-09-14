7c89b146cd4cc42b6b087e35bb75b0f74c43ae2d
yasser-osamaa <yasserosamaa88@gmail.com>
2024-09-14 17:51:25 +0300

    app bar of home screen

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -5,6 +5,42 @@ class HomeView extends StatelessWidget {
   static String id = 'HomeView';
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        clipBehavior: Clip.none,
+        title: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              '🍕PIZZA',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+              ),
+            ),
+            Text(
+              'Upgrade my plane',
+              style: TextStyle(
+                color: Color.fromARGB(255, 34, 133, 214),
+                fontWeight: FontWeight.bold,
+                fontSize: 17,
+              ),
+            )
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.short_text_rounded,
+              size: 50,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
