704365d2fbec63be8fb0c07fc66d1984c34ae617
yasser-osamaa <yasserosamaa88@gmail.com>
2024-09-15 16:00:43 +0300

    add image and price for detailes pizza view

--- lib/views/pizza_details_view.dart
+++ lib/views/pizza_details_view.dart
@@ -5,6 +5,85 @@ class PizzaDetailsView extends StatelessWidget {
   static String id = 'PizzaDetailes';
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 16),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: const Image(
+                image: NetworkImage(
+                    'https://media.istockphoto.com/id/1442417585/photo/person-getting-a-piece-of-cheesy-pepperoni-pizza.jpg?s=612x612&w=0&k=20&c=k60TjxKIOIxJpd4F4yLMVjsniB4W1BpEV4Mi_nb4uJU='),
+                fit: BoxFit.fill,
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(.1),
+                    offset: const Offset(0, 100),
+                    blurRadius: 200,
+                  ),
+                ],
+              ),
+              child: const Card(
+                elevation: 3,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 260,
+                            child: Text(
+                              'Truffle Temptation Extravaganza',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 23,
+                              ),
+                            ),
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                r'$80.00',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 23,
+                                  color: Colors.blue,
+                                ),
+                              ),
+                              Text(
+                                r'$85.00',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                  color: Colors.grey,
+                                  decoration: TextDecoration.lineThrough,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+//fit: BoxFit.fill,
