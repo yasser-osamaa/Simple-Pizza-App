c05177fa23e49dd0d72c0748137f0b70fd477b82
yasser-osamaa <yasserosamaa88@gmail.com>
2024-09-17 17:06:49 +0300

    create pizza and content model

---
+++ lib/constants.dart
@@ -0,0 +1,18 @@
+// menu collection
+const kMenuCollection = 'menu';
+
+// pizza model
+const kPizzaName = 'pizzaName';
+const kPizzaDesc = 'pizzaDesc';
+const kPizzaPrice = 'pizzaPrice';
+const kPizzaImg = 'pizzaImg';
+const kOptionImg = 'optionImg';
+const kSpicyType = 'spicyType';
+const kVegType = 'vegType';
+
+// content model
+const kContent = 'content';
+const kCaloires = 'caloires';
+const kCarp = 'carp';
+const kFat = 'fat';
+const kProtien = 'protien';
---
+++ lib/models/pizza_model.dart
@@ -0,0 +1,58 @@
+import 'package:pizza_app/constants.dart';
+
+class PizzaModel {
+  final String pizzaName;
+  final String pizzaDesc;
+  final String pizzaImg;
+  final String optionImg;
+  final String spicyType;
+  final String vegType;
+  final double pizzaPrice;
+  final ContentModel content;
+
+  PizzaModel(
+      {required this.pizzaName,
+      required this.pizzaDesc,
+      required this.pizzaImg,
+      required this.optionImg,
+      required this.spicyType,
+      required this.vegType,
+      required this.pizzaPrice,
+      required this.content});
+
+  factory PizzaModel.fromJson(jsonData) {
+    return PizzaModel(
+      pizzaName: jsonData[kPizzaName],
+      pizzaDesc: jsonData[kPizzaDesc],
+      pizzaImg: jsonData[kPizzaImg],
+      optionImg: jsonData[kOptionImg],
+      spicyType: jsonData[kSpicyType],
+      vegType: jsonData[kVegType],
+      pizzaPrice: jsonData[kPizzaPrice],
+      content: ContentModel.fromJson(
+        jsonData[kContent],
+      ),
+    );
+  }
+}
+
+class ContentModel {
+  final double caloires;
+  final double carp;
+  final double fat;
+  final double protien;
+
+  ContentModel(
+      {required this.caloires,
+      required this.carp,
+      required this.fat,
+      required this.protien});
+
+  factory ContentModel.fromJson(jsonData) {
+    return ContentModel(
+        caloires: jsonData[kCaloires],
+        carp: jsonData[kCarp],
+        fat: jsonData[kFat],
+        protien: jsonData[kProtien]);
+  }
+}
